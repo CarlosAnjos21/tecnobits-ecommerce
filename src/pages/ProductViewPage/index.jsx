680e63e586b46687e68259c9d49f3d765e83766a
Gabriel Mozart <antoiingabrielmozart@gmail.com>
2025-07-12 00:24:38 -0300

    reformulado as partes de divs e adicionado funcao para caulcular o desconto

--- src/pages/ProductViewPage/index.jsx
+++ src/pages/ProductViewPage/index.jsx
@@ -1,10 +1,19 @@
 import './ProductViewPage.css';
 import { useParams } from "react-router-dom";
 import products from '../../data/products.json';
+import { ButtonPrimary } from '../../components/Buttons/ButtonComponents';
+
+
+  
 
 function ProductViewPage() {
   const { id } = useParams();
   const product = products.find((p) => p.id === Number(id));
+
+  // Calcula o percentual de desconto do produto 
+  const descontoPercentual = product.priceDiscount && product.priceDiscount > 0
+    ? `${Math.round(((product.price - product.priceDiscount) / product.price) * 100)}%`
+    : "0%";
 
   if (!product) {
     return <h2>Produto não encontrado.</h2>;
@@ -12,16 +21,23 @@ function ProductViewPage() {
 
   return (
     <div className="principal">
-      <div className="esquerda">
-      <img
-        src={product.image}
-        alt={product.name} className='imagem-produto'
-      /></div>
-      <div className="direita">
-        <div className='conteudo'>
-      <p className='descricao'>{product.description}</p>
-      <p className="desconto"> De: {product.priceDiscount} por:</p>
-      <h2>R$ {product.price.toFixed(2)}</h2></div></div>
+      <div className="titulo">
+      <p>{product.category}/{product.brand}</p>
+      </div>
+      <div className='conteudo'>
+        <div className='esquerda'>
+          <img src={product.image} alt={product.name} className='imagem-produto' />
+        </div>
+        <div className='direita'>
+          <p>NOVO</p>
+          <h2 className='descricao'>{product.description}</h2>
+          <p className='desconto'>De: {product.price.toFixed(2)} por:</p>
+          <p className='preco'>R$ {product.priceDiscount}</p>
+          <p className='avista'><img src="/public/images/pix.png" alt="pix" className='pix'/>À vista no PIX com {descontoPercentual} de desconto</p>
+          <div className='botoes'><ButtonPrimary className='compraragr'>COMPRAR AGORA</ButtonPrimary>
+          <ButtonPrimary className='addcarrinho'>ADICIONAR AO CARRINHO</ButtonPrimary></div>
+        </div>
+      </div>
     </div>
   );
 }
