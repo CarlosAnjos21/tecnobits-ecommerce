868e745f64ae0089eb9cdcfc4b949a7f7d1cd5c1
Gabriel Mozart <antoiingabrielmozart@gmail.com>
2025-07-15 13:47:56 -0300

    header 100% finalizado

--- src/components/Header/HeaderComponent.jsx
+++ src/components/Header/HeaderComponent.jsx
@@ -4,13 +4,13 @@ import './Header.css'; // A usar CSS normal
 
 // Componentes que o seu Header precisa
 import Logo from '../Logo';
-import { PrimaryBtn } from '../Buttons/ButtonComponents';
 import { InputDefault } from '../Input';
 import { useCart } from '../../contexts/CartContext';
 // 1. Caminho da imagem corrigido para 'icons'
 import logoIcon from '../../assets/icons/logo.png';
 import products from '../../data/products.json'; // Importando os produtos para o filtro
-import { FaRegCircleUser } from 'react-icons/fa6';
+import { FaRegCircleUser, FaCartShopping, FaRegHeart } from 'react-icons/fa6';
+import { IoSunny } from 'react-icons/io5';
 
 const Header = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -196,13 +196,21 @@ const handleSearch = () => {
                                 <span className='user-line'><Link to='/create-account' className='user-link'>Cadastre-se</Link></span>
                                 </div>
                                 </div>
-
+                                
+                                <div className="functions">
+                                <Link to="/*" className="icon-link">
+                                    <IoSunny className='icon-sun' />
+                                </Link>
+                                <Link to="/*" className="icon-link">
+                                    <FaRegHeart className='icon-heart' />
+                                </Link>
                                 <Link to="/shopping-cart" className="icon-link">
-                                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M9 22C9.55228 22 10 21.5523 10 21C10 20.4477 9.55228 20 9 20C8.44772 20 8 20.4477 8 21C8 21.5523 8.44772 22 9 22ZM20 22C20.5523 22 21 21.5523 21 21C21 20.4477 20.5523 20 20 20C19.4477 20 19 20.4477 19 21C19 21.5523 19.4477 22 20 22ZM1 1H5L7.68 14.39C7.77144 14.8504 8.02191 15.264 8.38755 15.5583C8.75318 15.8526 9.2107 16.009 9.68 16H19.4C19.8693 16.009 20.3268 15.8526 20.6925 15.5583C21.0581 15.264 21.3086 14.8504 21.4 14.39L23 6H6" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/></svg>
-                                    {cartItemCount > 0 && (
+                                    <FaCartShopping className='cart-icon' />
+                                    {cartItemCount >= 0 && (
                                         <span className="cart-badge">{cartItemCount}</span>
                                     )}
                                 </Link>
+                                </div>
                             </div>
                         </div>
                         <nav className={navClasses}>
