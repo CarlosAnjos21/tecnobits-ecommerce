5a8b9e838be5e696559ae42f2227d0f08779d2d1
daniolivem <danielymelo@aluno.uespi.br>
2025-08-30 12:26:13 -0300

    chore(repo): commitar alterações locais - frontend UI, assets e ajustes iniciais

--- src/components/Input/index.jsx
+++
@@ -1,9 +0,0 @@
-import './Input.css';
-
-export const InputDefault = ({ className = '', $inputLogin, ...props }) => {
-  const inputClasses = `input-default ${$inputLogin ? 'input-login' : ''} ${className}`;
-  
-  return (
-    <input className={inputClasses} {...props} />
-  );
-};
