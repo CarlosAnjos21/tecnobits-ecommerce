dcfc455ecfce938d4cadbc8ddc1a93573be8733d
Anderson Guimarães <user.email>
2025-09-02 09:59:08 -0300

    fix(product-page): corrige visibilidade do botão de CEP no mobile

--- frontend/src/pages/ProductViewPage/index.jsx
+++ frontend/src/pages/ProductViewPage/index.jsx
@@ -154,9 +154,9 @@ function ProductViewPage() {
                               onChange={handleCepChange}
                               maxLength={9} // Formato XXXXX-XXX
                             />
-                            <ButtonPrimary className='botao-ok' onClick={handleCalculateShippingClick}>
-                              OK
-                            </ButtonPrimary>
+                            <ButtonPrimary className='botao-ok mobile' onClick={handleCalculateShippingClick}>
+  OK
+</ButtonPrimary>
                           </div>
                           {shippingMessage && <p className='valorfrete'>{shippingMessage}</p>}
                         </div>
