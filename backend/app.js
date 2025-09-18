888d4fe8c20b27c1c6ba87a8edc4c7cb2d97f099
daniolivem <danielymelo@aluno.uespi.br>
2025-09-18 17:47:01 -0300

    feat: Adicionar multer como dependência no backend

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ import productRoutes from './src/routes/productRoutes.js';
 import adminRoutes from './src/routes/adminRoutes.js';
 import categoryRoutes from "./src/routes/categoryRoutes.js";
 import cartRoutes from "./src/routes/cartRoutes.js";
-// import orderRoutes from "./src/routes/orderRoutes.js";
+import orderRoutes from "./src/routes/orderRoutes.js";
 import { protect, authorize } from './src/middleware/authMiddleware.js';
 
 
