3bfffa5ba214bb18dc14d77795f79af4c8aaad7c
Maycacon <mayconbarroso2@gmail.com>
2025-09-19 19:00:58 -0300

    integrando banco

--- backend/seeds/createProductsAndCategories.js
+++ backend/seeds/createProductsAndCategories.js
@@ -2,6 +2,8 @@ import { PrismaClient } from '@prisma/client';
 import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import dotenv from "dotenv";
+dotenv.config({ path: "./.env" });
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
